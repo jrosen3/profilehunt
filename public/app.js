a83db35fc3c54c8336c5e4eb6b4141853dbbe4fa
Ben Donald <benldonald@gmail.com>
2014-10-24 14:55:58 -0400

    init angularjs

---
+++ public/app.js
@@ -0,0 +1,7 @@
+angular.module('ProfileHunt',[])
+	.controller('test', ['$scope', function ($scope) {
+        $scope.name = 'Jared';
+      }])
+	.controller('header', ['$scope', function ($scope) {
+        $scope.name = 'Jared';
+      }]);
